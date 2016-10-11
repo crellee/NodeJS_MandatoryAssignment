aef808b961ad8c711b9b9865309de508ca10a4cc
christianhasselstrom <crellecrelle@gmail.com>
2016-10-11 09:15:43 +0200

    few small updates

--- Express.js
+++ Express.js
@@ -1,3 +1,6 @@
+//https://github.com/crellee/NodeJS_MandatoryAssignment.git
+//git repository
+
 var express = require('express');
 var app = express();
 
@@ -5,14 +8,27 @@ var fs = require('fs');
 
 app.set('view engine', 'ejs');
 
+var homepageinfo = {
+	title: 'Home',
+	subtitle: 'Homepage for the Mandatory Assignment',
+	text: 'this is some text i choose to put in this paragraph'
+};
+
+var aboutpageinfo = {
+	title: 'About',
+	subtitle: 'About page for the Mandatory Assignment',
+	text: 'This text comes from a json file !! :)'
+};
+
+
+fs.writeFileSync('./about.json', JSON.stringify(aboutpageinfo, null, 4) , 'utf-8');	
+
+
 app.get('/', function(req, res){
-	//res.send('<h1>Home Page</h1>');
 	res.render('index', homepageinfo);
 });
 
 app.get('/about', function(req, res){
-	//res.send('<h1>Contact Page</h1>');
-		//res.render('contact', objJSON);
 		fs.readFile('about.json', 'utf-8', function(err, data) {
 			res.render('index', JSON.parse(data));
 
@@ -21,15 +37,5 @@ app.get('/about', function(req, res){
 
 app.listen(3001);
 
-var homepageinfo = {
-	title: 'Home',
-	subtitle: 'Homepage',
-	text: 'this is some text i choose to put in this paragraph'
-};
 
-var aboutpageinfo = {
-	title: 'About',
-	subtitle: 'About page',
-	text: 'waiting for json'
 
-};
